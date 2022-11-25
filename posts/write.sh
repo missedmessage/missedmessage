de712c4cc1f5cb0b7b19f85f06a44052bc3a9aef
missedmessage <missedmessage@users.noreply.github.com>
2022-11-25 17:21:44 +0000

    deploy: b216befa4d45851e80836a8d35cdc2f9c93c95c5

--- posts/write.sh
+++ posts/write.sh
@@ -1,8 +1,11 @@
 cd "Github repo"/Blog/zozo2_hugo
-now=$(date "+%Y-%m-%d")
-str=$(md5 -s $now)
+now1=$(date "+%Y-%m-%dT%H:%M:%S+08:00")
+now2=$(date "+%Y %m %d")
+str=$(md5 -s $now1$now2)
 md5=${str#* = }
-hugo new posts/"$now.md"
-open /Users/wangshuai/GitHub\ repo/Blog/zozo2_hugo/content/posts/$now.md
+submd5=${md5:4:(12-4)}
+
+hugo new posts/"$submd5.md"
+open /Users/wangshuai/GitHub\ repo/Blog/zozo2_hugo/content/posts/$submd5.md
 cd content/posts
-mkdir "$md5"
+mkdir "$submd5"
